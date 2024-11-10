6390d5bb0b83131401245a9c96ea04b27ed8eeb7
Your Name <youremail@example.com>
2024-11-11 00:15:09 +0600

    database mongoose schema create

--- models/User.js
+++ models/User.js
@@ -1,30 +1,139 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: true,
-    },
-    password: {
-      type: String,
-    },
-    isVerified: {
-      type: Boolean,
-      default: false,
-    },
-    googleId: {
-      type: String,
-    },
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
   },
-  {
-    timestamps: true,
-  }
-);
+  email: {
+    type: String,
+    unique: true,
+  },
+  emailVerified: {
+    type: Date,
+  },
+  image: {
+    type: String,
+  },
+  accounts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Account",
+    },
+  ],
+  sessions: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Session",
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-const User = mongoose.models.users || mongoose.model("users", userSchema);
-export default User;
+const User = mongoose.models.User || mongoose.model("User", UserSchema);
+export { User };
+
+const AccountSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  type: {
+    type: String,
+  },
+  provider: {
+    type: String,
+  },
+  providerAccountId: {
+    type: String,
+  },
+  refresh_token: {
+    type: String,
+  },
+  access_token: {
+    type: String,
+  },
+  expires_at: {
+    type: Number,
+  },
+  token_type: {
+    type: String,
+  },
+  scope: {
+    type: String,
+  },
+  id_token: {
+    type: String,
+  },
+  session_state: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+AccountSchema.index({ provider: 1, providerAccountId: 1 }, { unique: true });
+
+const Account =
+  mongoose.models.Account || mongoose.model("Account", AccountSchema);
+
+export { Account };
+
+const SessionSchema = new mongoose.Schema({
+  sessionToken: {
+    type: String,
+    unique: true,
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  expires: {
+    type: Date,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Session =
+  mongoose.models.Session || mongoose.model("Session", SessionSchema);
+
+export { Session };
+
+const VerificationTokenSchema = new mongoose.Schema({
+  identifier: {
+    type: String,
+  },
+  token: {
+    type: String,
+  },
+  expires: {
+    type: Date,
+  },
+});
+
+VerificationTokenSchema.index({ identifier: 1, token: 1 }, { unique: true });
+
+const VerificationToken =
+  mongoose.models.VerificationToken ||
+  mongoose.model("VerificationToken", VerificationTokenSchema);
+
+export default VerificationToken;
